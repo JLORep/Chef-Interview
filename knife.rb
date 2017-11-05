de5c83e0e76ff715bac08ebbf24e714b4ef85d97
james <jameseymail@hotmail.co.uk>
2017-11-05 17:19:43 +0000

    all nodes now bootstrapped

--- knife.rb
+++ knife.rb
@@ -4,4 +4,4 @@ log_location             STDOUT
 node_name                "jamesey"
 client_key               "C:/chef/chef-repo/.chef/jamesey.pem"
 chef_server_url          "https://ec2-13-58-209-15.us-east-2.compute.amazonaws.com/organizations/jl"
-cookbook_path            ["#{current_dir}/../cookbooks"]
+cookbook_path            "C:/chef/chef-repo/cookbooks"
