cae7fdba144a181c2cb4ce8f3e7cf3d0af269349
james <jameseymail@hotmail.co.uk>
2017-11-05 18:51:50 +0000

    all prereqs installed, commit before install mysql supermarkey cookbook

--- cookbooks/lamp-stack/recipes/mysql.rb
+++ cookbooks/lamp-stack/recipes/mysql.rb
@@ -1,11 +1,13 @@
-mysql_service 'default' do
-  version '5.5'
+mysql_client 'default' do
+  action :create
+end
+
+mysql_service 'db_some_data' do
+  port '3306'
+  initial_root_password 'abc123'
   action [:create, :start]
 end
 
-mysql_config 'hello' do
-  instance 'default'
-  source 'hello.conf.erb'
-  version '5.5'
-  action :create
+mysql2_chef_gem 'default' do
+  action :install
 end
