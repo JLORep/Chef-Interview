a0d104d7a771bac04d8c8065be5f826f1e5f26ca
james <jameseymail@hotmail.co.uk>
2017-11-05 16:08:56 +0000

    initial commit post server & cleint installation

---
+++ knife.rb
@@ -0,0 +1,7 @@
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "jamesey"
+client_key               "C:/chef/chef-repo/.chef/jamesey.pem"
+chef_server_url          "https://ec2-13-58-209-15.us-east-2.compute.amazonaws.com/organizations/jl"
+cookbook_path            ["#{current_dir}/../cookbooks"]
